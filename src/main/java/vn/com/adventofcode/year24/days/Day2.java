52926161460ab4c4071d4c4ccb14da9843c131ca
AnhTuPhi <phianhtu2211@gmail.com>
2024-12-02 23:34:52 +0700

    solve day 2

--- src/main/java/vn/com/adventofcode/year24/days/Day2.java
+++ src/main/java/vn/com/adventofcode/year24/days/Day2.java
@@ -2,6 +2,8 @@ package vn.com.adventofcode.year24.days;
 
 import vn.com.adventofcode.year24.Day2024;
 
+import java.util.stream.IntStream;
+
 /**
  * @author : AnhTuPhi
  * @created : 12/1/2024 - 11:29 PM - Sunday
@@ -19,11 +21,41 @@ public class Day2 extends Day2024 {
 
     @Override
     public Object part1() {
-        return "";
+        return dayStream().filter(this::isSafeReport).count();
     }
 
     @Override
     public Object part2() {
-        return "";
+        return dayStream().filter(this::isSafeWithDampener).count();
+    }
+
+    private boolean isSafeReport(String report) {
+        String[] levels = report.split(" ");
+        for (int i = 1; i < levels.length; i++) {
+            int diff = Integer.parseInt(levels[i].replace("\r", "")) - Integer.parseInt(levels[i - 1]);
+            if (Math.abs(diff) < 1 || Math.abs(diff) > 3 || (diff > 0 && i > 1 && Integer.parseInt(levels[i - 1]) < Integer.parseInt(levels[i - 2])) || (diff < 0 && i > 1 && Integer.parseInt(levels[i - 1]) > Integer.parseInt(levels[i - 2]))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isSafeWithDampener(String report) {
+        if (isSafeReport(report)) {
+            return true;
+        }
+        String[] levels = report.split(" ");
+        return IntStream.range(0, levels.length)
+                .anyMatch(i -> isSafeReport(removeLevel(levels, i)));
+    }
+
+    private String removeLevel(String[] levels, int index) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < levels.length; i++) {
+            if (i != index) {
+                sb.append(levels[i]).append(" ");
+            }
+        }
+        return sb.toString().trim();
     }
 }
